ccfb4f2dec8bd90767e2e6bd1d5d074fd46cd4f8
Adriano Carlos Verona <adriano.verona@gmail.com>
2025-11-13 16:54:53 -0500

    updated to .net 9.0

--- RoslynSample/RoslynSample.cs
+++ RoslynSample/RoslynSample.cs
@@ -4,85 +4,93 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 using static System.Console;
 
-namespace RoslynSample
+namespace RoslynSample;
+
+public class Driver
 {
-    public class Driver
-    {
-	    static void Main()
-	    {
+	static void Main()
+	{
 
-			var syntaxTree = CSharpSyntaxTree.ParseText(@"class C { void Foo() {
-	int i = 10;  
-	i = i + 1; /* My Comment */ 
-} }");
+		var syntaxTree = CSharpSyntaxTree.ParseText("""
+		                                            class C 
+		                                            { 
+		                                                void Foo() 
+		                                                {
+		                                                    int i = 10; 
+		                                                    i = i + 1; /* My Comment */ 
+		                                                }
+		                                            }
+		                                            """);
 
-			var visitor = new MyVisitor();
-			visitor.Visit(syntaxTree.GetRoot());
-			
-		    var updater = new UpdaterVisitor();
-		    var updated = updater.Visit(syntaxTree.GetRoot());
+		//var visitor = new MyVisitorNonWalker();
+		var visitor = new MyVisitor();
+		visitor.Visit(syntaxTree.GetRoot());
+		
+		var updater = new UpdaterVisitor();
+		var updated = updater.Visit(syntaxTree.GetRoot());
+		
+		WriteLine($"\n{updated.ToFullString()}");
+	}
+}
 
-			WriteLine($"\n{updated.ToFullString()}");
-	    }
+internal class UpdaterVisitor : CSharpSyntaxRewriter
+{
+	public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+	{
+		var stringToken = SyntaxFactory.Token(SyntaxKind.StringKeyword)
+								.WithLeadingTrivia(SyntaxFactory.Space, SyntaxFactory.Space, SyntaxFactory.Space, SyntaxFactory.Space)
+								.WithTrailingTrivia(SyntaxFactory.Space);
+
+		var newField = SyntaxFactory.FieldDeclaration(
+			SyntaxFactory.VariableDeclaration(
+				SyntaxFactory.PredefinedType(stringToken),
+				SyntaxFactory.SeparatedList<VariableDeclaratorSyntax>().Add(SyntaxFactory.VariableDeclarator("foo")))).WithTrailingTrivia(SyntaxFactory.LineFeed);
+
+		var members = new SyntaxList<MemberDeclarationSyntax>();
+		members = members.Add(newField);
+
+		return node.WithMembers(members.AddRange(node.Members));
+	}
+}
+
+class MyVisitor : CSharpSyntaxWalker
+{
+	public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+	{
+		WriteLine($"class {node.Identifier}");
+		WriteLine("{");
+		base.VisitClassDeclaration(node);
+		WriteLine("}");
 	}
 
-	internal class UpdaterVisitor : CSharpSyntaxRewriter
+	public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
 	{
-		public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
-		{
-			var stringToken = SyntaxFactory.Token(SyntaxKind.StringKeyword).WithTrailingTrivia(SyntaxFactory.Space);
-
-			var newField = SyntaxFactory.FieldDeclaration(
-							SyntaxFactory.VariableDeclaration(
-								SyntaxFactory.PredefinedType(stringToken),
-								SyntaxFactory.SeparatedList<VariableDeclaratorSyntax>().Add(SyntaxFactory.VariableDeclarator("foo"))));
-
-			var members = new SyntaxList<MemberDeclarationSyntax>();
-			members = members.Add(newField);
-
-			return node.WithMembers(members);
-		}
+		Write($"\t{node.ReturnType} {node.Identifier}(");
+		node.ParameterList.Accept(this);
+		WriteLine(")");
+		WriteLine("\t{");
+		node.Body.Accept(this);
+		WriteLine("\t}");
 	}
 
-	class MyVisitor : CSharpSyntaxWalker
+	public override void VisitExpressionStatement(ExpressionStatementSyntax node)
 	{
-		public override void VisitClassDeclaration(ClassDeclarationSyntax node)
-		{
-			WriteLine($"class {node.Identifier}");
-			WriteLine("{");
-			base.VisitClassDeclaration(node);
-			WriteLine("}");
-		}
+		WriteNode(node);
+	}
 
-		public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
-		{
-			Write($"\t{node.ReturnType} {node.Identifier}(");
-			node.ParameterList.Accept(this);
-			WriteLine(")");
-			WriteLine("\t{");
-			node.Body.Accept(this);
-			WriteLine("\t}");
-		}
+	public override void VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
+	{
+		WriteNode(node);
+	}
 
-		public override void VisitExpressionStatement(ExpressionStatementSyntax node)
-		{
-			WriteNode(node);
-		}
+	private static void WriteNode(SyntaxNode node)
+	{
+		if (node.HasLeadingTrivia)
+			Write(node.GetLeadingTrivia().ToFullString());
 
-		public override void VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
-		{
-			WriteNode(node);
-		}
+		Write(node.ToString());
 
-		private static void WriteNode(SyntaxNode node)
-		{
-			if (node.HasLeadingTrivia)
-				Write(node.GetLeadingTrivia().ToFullString());
-
-			Write(node.ToString());
-
-			if (node.HasTrailingTrivia)
-				Write(node.GetTrailingTrivia().ToFullString());
-		}
+		if (node.HasTrailingTrivia)
+			Write(node.GetTrailingTrivia().ToFullString());
 	}
 }
